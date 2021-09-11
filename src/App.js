79fadc65bbe459de67a629d5caa85283362d5d6b
Diksha <dikshar73@gmail.com>
2021-09-11 16:28:30 +0530

    EntryPage for the restaurant app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import EntryLogo from "./assets/res2.jpeg";
+import Button from "react-bootstrap/Button";
+import  Container   from "react-bootstrap/Container";
 
 function App() {
+
+  const handleClick=()=>{
+
+  } // to create react-router for routing to homepage
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundImage: `url(${EntryLogo})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        position: "relative",
+        textAlign: "center",
+        color: "darkred",
+        fontFamily: "cursive",
+      }}
+    >
+      <div style={{ position: "absolute", top: "25%", left: "35%" }}>
+        <h1>Delish</h1>
+        <h3>...Relish your taste buds</h3>
+        <Container justify>
+        <Button variant='info' onClick={handleClick}>Take a tour</Button>{'  '}
+        <Button variant='info'>Login</Button>
+        </Container>
+      </div>
     </div>
   );
 }
